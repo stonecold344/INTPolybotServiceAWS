f2f48de63a000b7c6326c532edf8b9e2168a9a41
bennyi <stonecold344@gmail.com>
2024-09-13 19:53:19 +0300

    testing

--- polybot/app.py
+++ polybot/app.py
@@ -14,11 +14,12 @@ logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(
 
 # Load environment variables from the .env file
 load_dotenv(dotenv_path='/usr/src/app/.env')  # Update path if needed
-logging.info("Env file loaded")
+logging.info("Environment file loaded")
 
 app = flask.Flask(__name__)
 
 def get_secret(secret_id):
+    """Retrieve a secret from AWS Secrets Manager."""
     session = boto3.session.Session()
     logging.info("Session credentials: %s", session.get_credentials())
     client = session.client(service_name='secretsmanager', region_name='eu-west-3')
@@ -28,8 +29,8 @@ def get_secret(secret_id):
         secret = get_secret_value_response['SecretString']
         return json.loads(secret)
     except Exception as e:
-        logging.error(f"Error retrieving secret: {e}")
-        raise e
+        logging.error("Error retrieving secret: %s", e)
+        raise
 
 # Retrieve YOLO5 instance IP
 yolo5_instance_ips = []
@@ -41,14 +42,14 @@ for reservation in response['Reservations']:
         public_ip = instance.get('PublicIpAddress')
         if public_ip:
             yolo5_instance_ips.append(public_ip)
-            logging.info(f"Found YOLO5 instance IP: {public_ip}")
+            logging.info("Found YOLO5 instance IP: %s", public_ip)
 
 if yolo5_instance_ips:
     YOLO5_URL = f'http://{yolo5_instance_ips[0]}:8081'  # Use the first IP if there are multiple
-    logging.info(f"YOLO5 service URL: {YOLO5_URL}")
+    logging.info("YOLO5 service URL: %s", YOLO5_URL)
 else:
     logging.error("Could not find YOLO5 instance IP")
-    YOLO5_URL = None  # Handle the case where YOLO5_URL is not set
+    YOLO5_URL = None
 
 # Retrieve the Telegram token
 SECRET_ID = "telegram/token"
@@ -56,7 +57,7 @@ secrets = get_secret(SECRET_ID)
 TELEGRAM_TOKEN = secrets.get("TELEGRAM_TOKEN")
 
 # Use the token in your application
-logging.info(f"Telegram Token: {TELEGRAM_TOKEN}")
+logging.info("Telegram Token: %s", TELEGRAM_TOKEN)
 
 # Environment Variables
 TELEGRAM_APP_URL = os.getenv('TELEGRAM_APP_URL')
@@ -64,9 +65,8 @@ S3_BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
 DYNAMODB_TABLE = os.getenv('DYNAMODB_TABLE')
 AWS_REGION = os.getenv('AWS_REGION')
 SQS_URL = os.getenv('SQS_URL')
-logging.info(f"Env variables: TELEGRAM_TOKEN={TELEGRAM_TOKEN}, TELEGRAM_APP_URL={TELEGRAM_APP_URL}, "
-             f"S3_BUCKET_NAME={S3_BUCKET_NAME}, YOLO5_URL={YOLO5_URL}, DYNAMODB_TABLE={DYNAMODB_TABLE}, "
-             f"AWS_REGION={AWS_REGION}, SQS_URL={SQS_URL}")
+logging.info("Environment variables: TELEGRAM_TOKEN=%s, TELEGRAM_APP_URL=%s, S3_BUCKET_NAME=%s, YOLO5_URL=%s, DYNAMODB_TABLE=%s, AWS_REGION=%s, SQS_URL=%s",
+             TELEGRAM_TOKEN, TELEGRAM_APP_URL, S3_BUCKET_NAME, YOLO5_URL, DYNAMODB_TABLE, AWS_REGION, SQS_URL)
 
 # Ensure all environment variables are loaded
 if not all([TELEGRAM_TOKEN, TELEGRAM_APP_URL, S3_BUCKET_NAME, YOLO5_URL, DYNAMODB_TABLE, AWS_REGION, SQS_URL]):
@@ -81,13 +81,14 @@ table = dynamodb.Table(DYNAMODB_TABLE)
 bot = ObjectDetectionBot(TELEGRAM_TOKEN, TELEGRAM_APP_URL, S3_BUCKET_NAME, YOLO5_URL, AWS_REGION, SQS_URL)
 
 def set_webhook():
+    """Set the webhook for the Telegram bot."""
     retry_attempts = 3
     for attempt in range(retry_attempts):
         try:
             url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/getWebhookInfo"
-            responses = requests.get(url)
-            responses.raise_for_status()
-            webhook_info = responses.json()
+            response = requests.get(url)
+            response.raise_for_status()
+            webhook_info = response.json()
             logging.info("Webhook info: %s", webhook_info)
 
             current_url = webhook_info['result'].get('url', None)
@@ -100,28 +101,27 @@ def set_webhook():
                 logging.info("Setting webhook as it is not set or has a different URL. Current webhook URL: %s", current_url)
 
             set_url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/setWebhook"
-            responses = requests.post(set_url, data={"url": desired_url})
-            responses.raise_for_status()
-            result = responses.json()
+            response = requests.post(set_url, data={"url": desired_url})
+            response.raise_for_status()
+            result = response.json()
             logging.info("Set webhook response: %s", result)
 
             if result.get('ok'):
                 logging.info("Webhook set successfully")
+                return
             else:
                 logging.error("Failed to set webhook: %s", result)
-
-            return
 
         except requests.exceptions.HTTPError as http_err:
             if http_err.response.status_code == 429:
                 retry_after = int(http_err.response.headers.get('Retry-After', 1))
-                logging.info(f"Rate limit exceeded. Retrying after {retry_after} seconds...")
+                logging.info("Rate limit exceeded. Retrying after %d seconds...", retry_after)
                 time.sleep(retry_after)
             else:
-                logging.error(f"HTTP error occurred: {http_err}")
+                logging.error("HTTP error occurred: %s", http_err)
                 raise
         except Exception as e:
-            logging.error(f"Error occurred while setting webhook: {e}")
+            logging.error("Error occurred while setting webhook: %s", e)
             raise
 
 @app.route('/', methods=['GET'])
@@ -146,18 +146,18 @@ def results():
         return jsonify({'error': 'predictionId is required'}), 400
 
     try:
-        responses = table.get_item(Key={'prediction_id': prediction_id})
-        if 'Item' not in responses:
-            logging.error(f"Prediction not found for ID: {prediction_id}")
+        response = table.get_item(Key={'prediction_id': prediction_id})
+        if 'Item' not in response:
+            logging.error("Prediction not found for ID: %s", prediction_id)
             return jsonify({'error': 'Prediction not found'}), 404
-        prediction_summary = responses['Item']
+        prediction_summary = response['Item']
         chat_id = prediction_summary['chat_id']
         labels = prediction_summary['labels']
         text_results = '\n'.join([f"{label['class']} : {label['count']}" for label in labels])
         bot.send_text(chat_id, text_results)
         return 'Ok'
     except Exception as e:
-        logging.error(f"Error fetching prediction: {e}")
+        logging.error("Error fetching prediction: %s", e)
         return jsonify({'error': str(e)}), 500
 
 @app.route('/predict', methods=['POST'])
@@ -177,12 +177,12 @@ def predict():
             logging.error("YOLO5_URL is not set")
             return jsonify({'error': 'YOLO5 service URL is not available'}), 500
 
-        responses = requests.post(YOLO5_URL, json={"image_url": image_url})
-        if responses.status_code != 200:
-            logging.error(f"Error from YOLO5 service: {responses.text}")
+        response = requests.post(YOLO5_URL, json={"image_url": image_url})
+        if response.status_code != 200:
+            logging.error("Error from YOLO5 service: %s", response.text)
             return jsonify({'error': 'Error from YOLO5 service'}), 500
 
-        result = responses.json()
+        result = response.json()
         prediction_id = result.get('prediction_id')
         if not prediction_id:
             logging.error("Missing prediction_id in YOLO5 response")
@@ -191,7 +191,7 @@ def predict():
         return jsonify({'prediction_id': prediction_id}), 200
 
     except Exception as e:
-        logging.error(f"Error in /predict endpoint: {e}")
+        logging.error("Error in /predict endpoint: %s", e)
         return jsonify({'error': str(e)}), 500
 
 @app.route('/loadTest/', methods=['POST'])
